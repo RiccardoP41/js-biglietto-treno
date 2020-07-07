12ef8b94d2882620cece77f32293c3f8e56a76e0
Riccardo Pasini <riccardo.pasini1992@gmail.com>
2020-07-07 14:20:16 +0200

    ok

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ var eta = parseInt (prompt("Quanti anni hai?"))
 
 var pb = km * 0.21
 
+// ***prima soluzione***
 
 if ( eta<18 ){
     pb *= 0.8
@@ -12,6 +13,14 @@ if ( eta<18 ){
 else if ( eta>=65 ) {
     pb *= 0.6
 }
-console.log(pb);
+
+// ***seconda soluzione***
+
+// if (eta<18) {
+//     pb -= pb *20 /100
+// }
+// else if (eta>=65) {
+//     pb -= pb *40 /100
+// }
 
 document.getElementById("prezzo-biglietto").innerHTML = "Il tuo biglietto costa" + " " + pb + " " + "euro."
